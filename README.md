622956f32418512ab26097cea69b1e6d88835205
moon.wang <moon.wang@pkfare.com>
2017-09-29 19:06:16 +0800

    add antd

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ react manage bootstrap
 
 支持 eslint
 支持 pretitter
+
+
+react-app-rewired    Override create-react-app webpack configs without ejecting
+https://github.com/timarney/react-app-rewired
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { DatePicker } from 'antd';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+//
+ReactDOM.render(<DatePicker />, document.getElementById('root'));
+// registerServiceWorker();
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+// ReactDOM.render(<DatePicker />, mountNode);
