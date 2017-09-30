ac65e058a341c0e4eb7ffa9b85ab13569eb41740
moon.wang <moon.wang@pkfare.com>
2017-09-30 14:23:58 +0800

    引入图片

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,16 +1,16 @@
 import './style.css'
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import logo from './logo.svg'
 
 class Login extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          login
-        </p>
+        <p className="App-intro">login11</p>
+        <img src={logo} alt="Logo" />
       </div>
-    );
+    )
   }
 }
 
-export default Login;
+export default Login
